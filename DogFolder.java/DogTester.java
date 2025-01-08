85b653330cfd27acd79fcc48c33eda14bf64c985
coopshoot <shootc.54@gmail.com>
2025-01-08 14:29:17 -0600

    Create DogTester.java

---
+++ DogFolder.java/DogTester.java
@@ -0,0 +1,11 @@
+public class DogTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Dog karel = new Dog("Karel", "Retriever" , true);
+        Dog clover = new Dog("Clover", "Corgi");
+        System.out.println(karel);
+        System.out.println(clover);
+    }
+}
