2a638a8c24ae7624b87c01883ced627d19f15654
coopshoot <shootc.54@gmail.com>
2025-01-09 14:03:09 -0600

    Update StudentTester.java

--- StudentFolder.java/StudentTester.java
+++ StudentFolder.java/StudentTester.java
@@ -4,9 +4,10 @@ public class StudentTester
     {
         Student alan = new Student("Alan", "Turing", 11, "Liberty High School");
         Student ada = new Student("Ada", "Lovelace", 5);
+        StudentClub dinoClub = new StudentClub(alan, "Dino Club");
         
-        System.out.println(alan);
-        System.out.println(ada);
+        System.out.println(dinoClub);
+        
         
     }
 }
