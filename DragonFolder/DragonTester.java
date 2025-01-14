64eccc1d0737681a6ac8e7bd80391bc30320ec0d
coopshoot <shootc.54@gmail.com>
2025-01-14 14:02:19 -0600

    Create DragonTester.java

---
+++ DragonFolder/DragonTester.java
@@ -0,0 +1,11 @@
+public class DragonTester
+{
+    public static void main(String[] args)
+    {
+        // Create a Dragon here to test out the Dragon class!
+        Dragon guud = new Dragon (6, ">");
+        Dragon tyd = new Dragon (4, "i");
+        System.out.println(guud);
+        System.out.println(guud.fight());
+    }
+}
