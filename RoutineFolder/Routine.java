cfee831050b60dc8671df8724496910e358cf6db
coopshoot <shootc.54@gmail.com>
2025-01-16 14:04:29 -0600

    Create Routine.java

---
+++ RoutineFolder/Routine.java
@@ -0,0 +1,14 @@
+    public Routine(double _school, double _sleep, double _friends, double _hobbies)
+    {
+        school = _school;
+        sleep = _sleep;
+        friends = _friends;
+        hobbies = _hobbies;
+    }
+    public Routine()
+    {
+        school = 0.0;
+        sleep = 0.0;
+        friends = 0.0;
+        hobbies = 0.0;
+    }
