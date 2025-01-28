989a862997383610bcb1319d107d8be9322523e9
coopshoot <shootc.54@gmail.com>
2025-01-28 14:06:11 -0600

    Create SongTester.java

---
+++ SongFolder/SongTester.java
@@ -0,0 +1,13 @@
+public class SongTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Song abba = new Song("ABBA", "Dancing Queen", 3, 54);
+        Song bruce = new Song("Bruce Springsteen", "The Heart of Rock and Roll", 5, 14);
+        Song huey = new Song("Huey Lewis & the News", "The Heart of Rock and Roll", 4, 59);
+        System.out.println(abba + "\n");
+        System.out.println(bruce + "\n");
+        System.out.println(huey + "\n");
+    }
+}
