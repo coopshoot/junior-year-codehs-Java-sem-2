d56e0258626018b5ed10fe4894c7e0e0ebfb3ec8
coopshoot <shootc.54@gmail.com>
2025-03-12 14:23:21 -0500

    Create ArrayListSearch.java

---
+++ ArrayListSearch.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class ArrayListSearch
+{
+    public static void main(String[] args) {
+        ArrayList<Double> array = new ArrayList<Double>();
+        array.add(4.5);
+        array.add(6.7);
+        array.add(5.0);
+        array.add(2.9);
+        array.add(7.0);
+        System.out.println(search(array, 5.0));
+    }
+
+    public static int search(ArrayList<Double> array, double key)
+    {
+        int i = 0;
+        while(i < array.size())
+        {
+            double ele = array.get(i);
+            if(ele == key)
+            {
+                return i;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return -1;
+    }
+}
