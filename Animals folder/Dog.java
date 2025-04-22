13da23a720cfb667ab80b15c7504dd8f5eade556
coopshoot <shootc.54@gmail.com>
2025-04-22 14:19:11 -0500

    Create Dog.java

---
+++ Animals folder/Dog.java
@@ -0,0 +1,14 @@
+public class Dog extends Pet
+{
+   private boolean trained;
+   
+   public boolean getTrained()
+   {
+       return trained;
+   }
+   public void setTrained(boolean x)
+   {
+       trained = x;
+   }
+   
+}
