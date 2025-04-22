0c2335fc296d7b5d7c8ce7274d83f573aa8c1a5f
coopshoot <shootc.54@gmail.com>
2025-04-22 14:19:22 -0500

    Create Fish.java

---
+++ Animals folder/Fish.java
@@ -0,0 +1,13 @@
+public class Fish extends Pet
+{
+    private String water;
+    
+    public String getWater()
+    {
+        return water;
+    }
+    public void setWater(String x)
+    {
+        water = x;
+    }
+}
