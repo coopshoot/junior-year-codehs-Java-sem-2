6a93e6524a0e1f1b9f4b24b419af1797a1e405f4
coopshoot <shootc.54@gmail.com>
2025-04-22 14:09:49 -0500

    Create Computer.java

---
+++ Computer folder/Computer.java
@@ -0,0 +1,26 @@
+public class Computer
+{
+    private int memory;
+    private int screenSize;
+    
+    public int getMemory()
+    {
+        return memory;
+    }
+    
+    public void setMemory(int x)
+    {
+        memory = x;
+    }
+    
+    public int getScreenSize()
+    {
+        return screenSize;
+    }
+    
+    public void setScreenSize(int j)
+    {
+        screenSize = j;
+    }
+    
+}
