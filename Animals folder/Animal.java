c7edb4566d64410b22e94165c727d0cefb2c82c9
coopshoot <shootc.54@gmail.com>
2025-04-22 14:18:48 -0500

    Create Animal.java

---
+++ Animals folder/Animal.java
@@ -0,0 +1,14 @@
+public class Animal
+{
+   private String type;
+   
+   public String getType()
+   {
+       return type;
+   }
+   public void setType(String x)
+   {
+       type = x;
+   }
+   
+}
