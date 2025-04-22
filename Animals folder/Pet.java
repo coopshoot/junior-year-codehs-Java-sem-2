387ccc8f4c00b1fa46728f5d7039a42a58fb61ae
coopshoot <shootc.54@gmail.com>
2025-04-22 14:19:33 -0500

    Create Pet.java

---
+++ Animals folder/Pet.java
@@ -0,0 +1,23 @@
+public class Pet extends Animal
+{
+    private String name;
+    private String size;
+    
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String x)
+    {
+        name = x;
+    }
+    public String getSize()
+    {
+        return size;
+    }
+    public void setSize(String r)
+    {
+        size = r;
+    }
+   
+}
