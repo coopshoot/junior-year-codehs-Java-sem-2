0ba8c326acacfbe5d634ac9dc87cf4fa8020e79c
coopshoot <shootc.54@gmail.com>
2025-04-23 14:29:17 -0500

    Create DogTester.java

---
+++ Dog Speak Folder/DogTester.java
@@ -0,0 +1,12 @@
+public class DogTester
+{
+    public static void main(String[] args)
+    {
+        // Start here
+        Dog dog = new Dog("name");
+        LoudDog loudDog = new LoudDog("Name");
+        
+        System.out.println(dog);
+        System.out.println(loudDog); 
+    }
+}
