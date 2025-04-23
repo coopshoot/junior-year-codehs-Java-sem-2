48c55193e294b3d7ebf77a1a351f51370353eb8f
coopshoot <shootc.54@gmail.com>
2025-04-23 14:12:54 -0500

    Create HealthyFood.java

---
+++ Foods folder/HealthyFood.java
@@ -0,0 +1,21 @@
+public class HealthyFood extends Food
+{
+    private String group;
+    private String color;
+    
+    public HealthyFood(String foodName, int calories, String foodGroup, String foodColor)
+    {
+        super(foodName,calories);
+        group = foodGroup;
+        color = foodColor;
+        
+    }
+    public String getGroup()
+    {
+        return group;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+}
