6b9cdc7b1017c156b22e921db0e4adbcc1c19789
coopshoot <shootc.54@gmail.com>
2025-04-23 14:20:20 -0500

    Create Clothing.java

---
+++ Clothing Folder/Clothing.java
@@ -0,0 +1,20 @@
+public class Clothing
+{
+    // Your code here
+    private String size;
+    private String color;
+    
+    public Clothing(String size, String color)
+    {
+        this.size = size;
+        this.color = color;
+    }
+    public String getSize()
+    {
+        return size;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+}
