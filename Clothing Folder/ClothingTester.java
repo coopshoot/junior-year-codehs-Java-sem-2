79194157275c1984d5c83765293c8ff454afc396
coopshoot <shootc.54@gmail.com>
2025-04-23 14:20:32 -0500

    Create ClothingTester.java

---
+++ Clothing Folder/ClothingTester.java
@@ -0,0 +1,15 @@
+public class ClothingTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Sweatshirt hoodie = new Sweatshirt("large", "white", true);
+        TShirt tee = new TShirt("Small", "blue", "cotton");
+        Jeans levi = new Jeans("medium");
+        Clothing hat = new Clothing("small", "red");
+        System.out.println(hoodie.hasHood());
+        System.out.println(tee.getColor());
+        System.out.println(levi.getSize());
+        System.out.println(tee.getFabric());
+    }
+}
