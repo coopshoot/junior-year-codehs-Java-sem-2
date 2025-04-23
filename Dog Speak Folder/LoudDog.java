24682897469ce1a225042c68490e98b1fcb5f44a
coopshoot <shootc.54@gmail.com>
2025-04-23 14:29:29 -0500

    Create LoudDog.java

---
+++ Dog Speak Folder/LoudDog.java
@@ -0,0 +1,20 @@
+public class LoudDog extends Dog
+{
+   
+   public LoudDog(String name){
+       super(name);
+   }
+   
+   // Override the speak method here
+   public String speak()
+   {
+       return "BARK!";
+   }   
+   public String toString()
+   {
+       return super.getName() +" is loud and likes to " + speak();
+   }
+   
+   //Override the toString here.
+   //Remember, you can access the name using super.getName()
+}
