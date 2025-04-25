6f4d624b0958eed203e8e8b1692e485a3eb9705a
coopshoot <shootc.54@gmail.com>
2025-04-25 13:57:03 -0500

    Create SquareTester.java

---
+++ Shapes folder/SquareTester.java
@@ -0,0 +1,12 @@
+public class SquareTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Rectangle rect = new Rectangle(100, 20);
+        Square squ = new Square(200);
+        
+        System.out.println(rect);
+        System.out.println(squ);
+    }
+}
